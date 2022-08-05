9d3afa8ee715423eb2cbd600c95e018db14b795e
Sein Izumita <sein.izumita@gmail.com>
2022-08-04 21:22:25 -0400

    create and generation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { LabelComponent } from './label/label.component';
+import { ButtonComponent } from './button/button.component';
+import { InputComponent } from './input/input.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LabelComponent,
+    ButtonComponent,
+    InputComponent
   ],
   imports: [
     BrowserModule
