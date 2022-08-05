5625dae96b7f0932694771535b66f30ffdc55426
Sein Izumita <sein.izumita@gmail.com>
2022-08-05 00:04:33 -0400

    adding components and formatting

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { LabelComponent } from './components/label/label.component';
 import { ButtonComponent } from './components/button/button.component';
 import { InputComponent } from './components/input/input.component';
+import { OutputComponent } from './output/output.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LabelComponent,
     ButtonComponent,
-    InputComponent
+    InputComponent,
+    OutputComponent
   ],
   imports: [
     BrowserModule
