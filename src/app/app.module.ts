fcab450200b49b6c4dc3356ebc9c71464e0db133
Sein Izumita <sein.izumita@gmail.com>
2022-08-04 21:22:32 -0400

    same

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,9 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { LabelComponent } from './label/label.component';
-import { ButtonComponent } from './button/button.component';
-import { InputComponent } from './input/input.component';
+import { LabelComponent } from './components/label/label.component';
+import { ButtonComponent } from './components/button/button.component';
+import { InputComponent } from './components/input/input.component';
 
 @NgModule({
   declarations: [
